29e07e02d09cba574e5b59c4abd28cd21241da00
JakubDrobnik <drobnik.j@gmail.com>
2018-11-20 15:29:46 +0100

    Set up input schema

--- README.md
+++ README.md
@@ -22,7 +22,6 @@ Example input:
 On this input actor searches places on this start url: https://www.google.com/maps/search/%C4%8Dsob/@50.0860729,14.4135326,10z
 
 - `searchString` - String will be search on Google maps
-- `searchViewport` - Object where user can define start view port for searching (Google will search places in this area)
-- `searchViewport.lat` - Viewport latitude
-- `searchViewport.lng` - Viewport longitude
-- `searchViewport.zoom` - Viewport zoom, e.g zoom: 10 -> https://www.google.com/maps/@50.0860729,14.4135326,10z vs zoom: 1 -> https://www.google.com/maps/@50.0860729,14.4135326,10z
+- `lat` - Viewport latitude
+- `lng` - Viewport longitude
+- `zoom` - Viewport zoom, e.g zoom: 10 -> https://www.google.com/maps/@50.0860729,14.4135326,10z vs zoom: 1 -> https://www.google.com/maps/@50.0860729,14.4135326,10z
--- src/main.js
+++ src/main.js
@@ -30,16 +30,16 @@ const enqueueAllUrlsFromPagination = async (page, requestQueue) => {
 };
 
 Apify.main(async () => {
-    const { searchString, searchViewport } = await Apify.getValue('INPUT');
+    const { searchString, lat, lng } = await Apify.getValue('INPUT');
 
     if (!searchString) throw new Error('Attribute searchString missing in input.');
 
     console.log('Scraping Google Places for search string:', searchString);
 
     let startUrl;
-    if (searchViewport) {
-        const { lat, lng, zoom = 10 } = searchViewport;
-        if (!lat || !lng) throw new Error('You have to defined lat and lng for searchViewport!');
+    if (lat || lng) {
+        const { zoom = 10 } = input;
+        if (!lat || !lng) throw new Error('You have to defined lat and lng!');
         startUrl = `https://www.google.com/maps/@${lat},${lng},${zoom}z/search`;
     } else {
         startUrl = 'https://www.google.com/maps/search/';
